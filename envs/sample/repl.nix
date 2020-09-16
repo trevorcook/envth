07c461a1713a60542805afea6f78b821e11ae52e
trevor cook <trevor.j.cook.civ@mail.mil>
2020-09-16 10:33:16 -0400

    WIP: part 2, patch fetchGit revisions to prev commit

--- envs/sample/repl.nix
+++ envs/sample/repl.nix
@@ -3,7 +3,7 @@ let
   env-th-overlay = self: super: { env-th = import env-th-src self super; };
   env-th-src = builtins.fetchGit {
       url = https://github.com/trevorcook/env-th.git ;
-      rev = "b4836bae89263d9cc70e883f21fffaed9e296272"; };
+      rev = "4bbaa985ead72d0e4fa3baed6b8c5e98f5255d28"; };
 in rec {
   inherit nixpkgs;
   inherit (nixpkgs) lib callPackage env-th;
--- envs/sample/sample.nix
+++ envs/sample/sample.nix
@@ -3,7 +3,7 @@ let
   env-th-overlay = self: super: { env-th = import env-th-src self super; };
   env-th-src = builtins.fetchGit {
       url = https://github.com/trevorcook/env-th.git ;
-      rev = "b4836bae89263d9cc70e883f21fffaed9e296272"; };
+      rev = "4bbaa985ead72d0e4fa3baed6b8c5e98f5255d28"; };
 in
 {env-th ? nixpkgs.env-th }:
 with env-th.addEnvs [ ./env-a.nix ./env-b.nix]; #Add "env-a" to env-th.envs;
