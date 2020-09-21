02092438a8cd5f380c68320adf1754d23a23be8c
trevor cook <trevor.j.cook.civ@mail.mil>
2020-09-21 14:58:21 -0400

    made python envs overridable

--- envs/python/python.nix
+++ envs/python/python.nix
@@ -5,22 +5,24 @@ let
                  "python3" "python35" "python36" "python37" "python38" "python39"
                ];
   make-python-version = name:
-    let pyenv = getAttr name self;
+    let pyenv = (getAttr name self).withPackages (_:[]);
     in { inherit name;
-      value = make-python-env name (pyenv.withPackages (_:[]));
-    };
-  make-python-env = name: pyenv: with self.env-th;
-    mkEnvironment rec {
-      inherit name;
-      definition = ./python-env.nix;
-      buildInputs = [] ;
-      passthru = rec {
-        env = pyenv;
-        addPackages = f: let
-          new-pyenv = pyenv.override (old: {
-            extraLibs = old.extraLibs ++ (f pyenv.pkgs);});
-          in make-python-env name new-pyenv;
-      };
-    };
+         value = make-python-env { inherit name pyenv;};
+       };
+  make-python-env = makeOverridable ({ name, pyenv}: with self.env-th;
+    let
+      pyenv-old = pyenv;
+      thisEnv = mkEnvironment {
+        inherit name;
+        definition = ./python-env.nix;
+        passthru = {
+          env = pyenv-old;
+          addPackages = f: let
+            pyenv = pyenv-old.override (old: {
+              extraLibs = old.extraLibs ++ (f pyenv-old.pkgs);});
+            in make-python-env {inherit name pyenv;};
+          };
+        };
+    in thisEnv);
 
 in listToAttrs (map make-python-version pyVersions)
