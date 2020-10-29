f56c3d2dbe4a4efeb7c023073cf12e77d10a6fa2
Trevor Cook <trevor.j.cook@gmail.com>
2020-10-28 20:04:11 -0400

    envs.haskell.ghc.addDeps: adds dependencies to environment based on cabal2nix style file.

--- envs/development/haskell/ghc-env.nix
+++ envs/development/haskell/ghc-env.nix
@@ -1,26 +1,27 @@
 { envth, name, selectFrom, haskellPackages, cabal2nix
-, package-cabal ? null# Enter the .cabal package
-, withHoogle?true
-}:
+, withHoogle?true, lib }: with lib; with builtins;
 let ghc = haskellPackages.ghc.withPackages selectFrom;
     ghc-dev = haskellPackages.ghc.withHoogle selectFrom;
     ghc-on-path = if withHoogle then ghc-dev else ghc;
-    /* cabal-dir =  */
+    mk-select = pkg-nix:
+      let deps = attrNames (functionArgs (import pkg-nix));
+        pre-filter = filter (pk: pk != "stdenv");
+        post-filter = filter (x: all (y: typeOf x != y) ["null" "lambda"]);
+        pick-dep = pkgs: name: getAttr name pkgs;
+      in pkgs: post-filter (map (pick-dep pkgs) (pre-filter deps));
 in envth.mkEnvironment rec {
   inherit name;
   definition = ./ghc-env.nix;
   paths = [cabal2nix ghc-on-path];
-  passthru = {
-    inherit selectFrom haskellPackages;
-    inherit ghc ghc-dev;
+  passthru = rec {
+    inherit selectFrom haskellPackages ghc ghc-dev;
+    #makes new ghc environment with some added packages;
     addPackages = f: let
       next-selectFrom = pkgs: (selectFrom pkgs) ++ (f pkgs);
       in import definition
-             { inherit name envth haskellPackages;
+             { inherit name envth haskellPackages withHoogle cabal2nix lib;
                 selectFrom = next-selectFrom; };
-    };
-  envlib = {
-    haskell-reload-env-with-updated-cabal = ''
-       cabal2nix . '';
+    # make new ghc environment with packages added from cabal2nix definition;
+    addDeps = pkg-nix: addPackages (mk-select pkg-nix);
     };
   }
